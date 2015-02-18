0e168e6ffbf89c79012a5fe04e96d7bc723f5f45
Larry <Larry@Macintoshs-MacBook-Air.local>
2015-02-18 14:29:15 -0800

    WDI Unit 5 Homework

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -120,4 +120,48 @@ function playToFive() {
     //  console.log("The score is currently " + playerWins + " to " + computerWins + "\n");
     return [playerWins, computerWins];
 }
-playToFive();
+
+function playTo(x) {
+    console.log("Let's play Rock Paper Scissors");
+    console.log("First one to " + x + " wins!");
+    var playerWins = 0;
+    var computerWins = 0;
+    var tiedGames = 0;
+    // This function should continue to play Rock Paper Scissors until either the player or the computer has won five times.
+    while (playerWins < x && computerWins < x) {
+        var playerMove = getPlayerMove();
+        var computerMove = getComputerMove();
+        var winner = getWinner(playerMove, computerMove);
+        if (winner == "player") {
+            playerWins += 1;
+        }
+        else if (winner == "computer") {
+            computerWins += 1;
+        }
+        else if (winner == "tie") {
+            tiedGames += 1;
+        }
+        else {
+            winner = null;
+        }
+        console.log("Player chose " + playerMove + " while Computer chose " + computerMove + ".");
+        if (winner == "computer") {
+            console.log("Computer wins");
+        }
+        else if (winner == "player") {
+            console.log("Player wins")
+        }
+        else if (winner == "tie") {
+            console.log("Tie game")
+        }
+        else {
+            console.log("Invalid entry")
+        }
+        console.log("The score is currently " + playerWins + " to " + computerWins + "\n");
+    }
+    // After each 'round', display some text in the console indicating who played what, who won, and what the current scoreboard looks like.
+    // For example,
+    //  console.log("Player chose " + playerMove + " while Computer chose " + computerMove);
+    //  console.log("The score is currently " + playerWins + " to " + computerWins + "\n");
+    return [playerWins, computerWins];
+}
